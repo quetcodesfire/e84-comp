89a7799c56879ddcfb5627e6af6dcf2d81ee75e9
marquet <marquet@element84.com>
2019-04-12 13:14:37 -0400

    did things

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi Colby, this is Marquet.</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
